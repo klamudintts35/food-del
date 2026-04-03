826077e0614b60525bf99b363cc2b7043337f7c6
klamudintts35 <mklamuddin0786@gmail.com>
2026-04-03 15:12:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 export const connectDB = async ()=> {
-    await mongoose.connect('mongodb+srv://klamudinm35_db_user:Klamudindb786@cluster0.pgfymrd.mongodb.net/food-del').then(()=> { console.log("DB connected")})
+    await mongoose.connect('mongodb+srv://klamudinm35_db_user:Klamudindb786@cluster0.rlza1is.mongodb.net/food-del').then(()=> { console.log("DB connected")})
 }
